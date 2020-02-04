b5bbef2b85975896b81b100d1680a964e4ba277d
sparklfox <actuallyori@gmail.com>
2020-02-04 22:29:20 +0000

    De-dupe guild listener

--- bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/discord/listeners/GuildEventListener.java
+++ bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/discord/listeners/GuildEventListener.java
@@ -4,6 +4,9 @@ import com.dumbdogdiner.betterwhitelist_bungee.BetterWhitelistBungee;
 import com.dumbdogdiner.betterwhitelist_bungee.discord.WhitelistBot;
 import com.dumbdogdiner.betterwhitelist_bungee.utils.PluginConfig;
 import com.dumbdogdiner.betterwhitelist_bungee.utils.SQLConnection;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildBanEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -18,28 +21,29 @@ import javax.annotation.Nonnull;
 public class GuildEventListener extends ListenerAdapter {
     @Override
     public void onGuildMemberLeave(@Nonnull GuildMemberLeaveEvent e) {
-        var id = e.getMember().getId();
-
-        // Disconnect the player if they are connected.
-        var playerUuid = SQLConnection.getUuidFromDiscordId(id);
-        if (playerUuid != null) {
-            var player = BetterWhitelistBungee.getInstance().getProxy().getPlayer(playerUuid);
-            if (player != null) {
-                player.disconnect(new TextComponent(ChatColor.RED + "You have left " +
-                    e.getGuild().getName() +
-                    ", and so have been unwhitelisted from the network!"));
-            }
-        }
-
-        if (SQLConnection.removeEntry(id)) {
-            WhitelistBot.getLogger().info("[discord][leave] Removed player with Discord ID '" + id + "' from the whitelist.");
-        }
+        disconnectWithMessage(
+            e.getGuild(),
+            e.getUser().getId(),
+            ChatColor.RED + "You have left " + e.getGuild().getName() + " and have been removed from the whitelist!"
+        );
     }
 
     @Override
     public void onGuildBan(GuildBanEvent e) {
-        var id = e.getUser().getId();
+        disconnectWithMessage(
+                e.getGuild(),
+                e.getUser().getId(),
+                ChatColor.RED + "You were banned from " + e.getGuild().getName() + " and have been removed from the whitelist!"
+        );
+    }
 
+    /**
+     * Disconnect a Discord member (if they are connected) with a given message.
+     * @param guild
+     * @param id
+     * @param message
+     */
+    private static void disconnectWithMessage(Guild guild, String id, String message) {
         if (!PluginConfig.getConfig().getBoolean("enableBanSync")) {
             WhitelistBot.getLogger().info("[discord] Not removing user '" + id + "' from whitelist - enableBanSync=false");
             return;
@@ -50,9 +54,7 @@ public class GuildEventListener extends ListenerAdapter {
         if (playerUuid != null) {
             var player = BetterWhitelistBungee.getInstance().getProxy().getPlayer(playerUuid);
             if (player != null) {
-                player.disconnect(new TextComponent(ChatColor.RED + "You have been banned from " +
-                        e.getGuild().getName() +
-                        ", and so have been unwhitelisted from the network!"));
+                player.disconnect(new TextComponent(message));
             }
         }
 
