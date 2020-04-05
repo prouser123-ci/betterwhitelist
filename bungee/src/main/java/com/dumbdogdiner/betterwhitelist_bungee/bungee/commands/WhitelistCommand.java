34a8399ec9be804f7bd34f9c5a05ff73780bb534
skyefoxie <actuallyori@gmail.com>
2020-04-05 20:11:02 +0100

    hotfix for bungee commands

--- bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/bungee/commands/WhitelistCommand.java
+++ bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/bungee/commands/WhitelistCommand.java
@@ -6,18 +6,25 @@ import com.dumbdogdiner.betterwhitelist_bungee.utils.UsernameValidator;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class WhitelistCommand extends Command {
 
     public WhitelistCommand() {
-        super("whitelist");
+        super("btw_whitelist");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof ProxiedPlayer) {
+            sender.sendMessage(new TextComponent(ChatColor.RED + "This command can only be run from the console."));
+            return;
+        }
+
         if (args.length < 1) {
-            sender.sendMessage(new TextComponent(ChatColor.RED + "Invalid arguments - syntax: <username> <discord_id>"));
+            sender.sendMessage(
+                    new TextComponent(ChatColor.RED + "Invalid arguments - syntax: <username> <discord_id>"));
             return;
         }
 
@@ -25,17 +32,19 @@ public class WhitelistCommand extends Command {
         var discordId = args.length > 1 ? args[1] : "none";
 
         if (user == null || user.id == null) {
-            sender.sendMessage(new TextComponent(ChatColor.RED + "Unable to find a user with name '" + args[0] +"'."));
+            sender.sendMessage(new TextComponent(ChatColor.RED + "Unable to find a user with name '" + args[0] + "'."));
             return;
         }
 
         if (SQLConnection.getDiscordIDFromMinecraft(user.id) != null) {
-            sender.sendMessage(new TextComponent(ChatColor.RED + "Player " + args[0] +" is already whitelisted. Run 'unwhitelist <player_name>' to remove them."));
+            sender.sendMessage(new TextComponent(ChatColor.RED + "Player " + args[0]
+                    + " is already whitelisted. Run 'unwhitelist <player_name>' to remove them."));
             return;
         }
 
         if (SQLConnection.addEntry(discordId, user.id)) {
-            sender.sendMessage(new TextComponent(ChatColor.AQUA + "Mapped user " + user.name +" to Discord ID '" + discordId +"'."));
+            sender.sendMessage(new TextComponent(
+                    ChatColor.AQUA + "Mapped user " + user.name + " to Discord ID '" + discordId + "'."));
         } else {
             sender.sendMessage(new TextComponent(ChatColor.RED + "Failed to map user - SQL update failed."));
         }
--- bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/bungee/commands/WhoisCommand.java
+++ bungee/src/main/java/com/dumbdogdiner/betterwhitelist_bungee/bungee/commands/WhoisCommand.java
@@ -11,11 +11,16 @@ import net.md_5.bungee.api.plugin.Command;
 public class WhoisCommand extends Command {
 
     public WhoisCommand() {
-        super("whois");
+        super("btw_whois");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof ProxiedPlayer) {
+            sender.sendMessage(new TextComponent(ChatColor.RED + "This command can only be run from the console."));
+            return;
+        }
+
         if (args.length == 0 || args[0] == null || args.length > 1) {
             sender.sendMessage(new TextComponent(ChatColor.RED + "Command Syntax: whois <uuid/discord/name>"));
         }
@@ -27,10 +32,8 @@ public class WhoisCommand extends Command {
 
         ProxiedPlayer player = null;
 
-        if (
-                target.matches("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}") ||
-                target.matches("[a-zA-Z0-9_]{1,16}")
-        ) {
+        if (target.matches("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
+                || target.matches("[a-zA-Z0-9_]{1,16}")) {
             player = BetterWhitelistBungee.getInstance().getProxy().getPlayer(target);
         } else {
             playerUuid = SQLConnection.getUuidFromDiscordId(target);
@@ -46,9 +49,12 @@ public class WhoisCommand extends Command {
         }
 
         // Feel like this could be shortened.
-        sender.sendMessage(new TextComponent(ChatColor.BLUE + "Information for user: " + ChatColor.WHITE + player.getName()));
-        sender.sendMessage(new TextComponent(ChatColor.BLUE + "UUID: " + ChatColor.WHITE + player.getUniqueId().toString()));
+        sender.sendMessage(
+                new TextComponent(ChatColor.BLUE + "Information for user: " + ChatColor.WHITE + player.getName()));
+        sender.sendMessage(
+                new TextComponent(ChatColor.BLUE + "UUID: " + ChatColor.WHITE + player.getUniqueId().toString()));
         sender.sendMessage(new TextComponent(ChatColor.BLUE + "Discord ID: " + ChatColor.WHITE + discordId));
-        sender.sendMessage(new TextComponent(ChatColor.BLUE + "Whitelisted: " + ChatColor.WHITE + (discordId == null ? "Yes" : "No")));
+        sender.sendMessage(new TextComponent(
+                ChatColor.BLUE + "Whitelisted: " + ChatColor.WHITE + (discordId == null ? "Yes" : "No")));
     }
 }
